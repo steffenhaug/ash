a76f1c3f4cfb685a9d7ff1fa0450a73c2830eaac
Steffen <steffen@haug.rs>
2021-03-15 21:00:18 +0100

    some error hadndling

--- ash.c
+++ ash.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <string.h>
+#include <signal.h>
 
 #include "rl/rl.h"
 #include "ash.h"
@@ -10,9 +11,10 @@
 
 void exec(struct command *cmd)
 {
-    // Internal commands.
     if (!strcmp("cd", cmd->prog)) {
-        chdir(cmd->args[1]);
+        if(-1 == chdir(cmd->args[1])) {
+            perror("Error (chdir)");
+        }
         return;
     }
 
@@ -23,32 +25,60 @@ void exec(struct command *cmd)
     // External comand; fork and exec.
     pid_t pid = fork();
 
-    if (!pid) { /* Child */
+    if (-1 == pid) {
+        perror("Error (fork)");
+        exit(1);
+    }
 
+    if (!pid) { /* Child */
         // Input redirection
         if (cmd->in) {
             int fd = open(cmd->in, O_RDONLY);
-            dup2(fd, STDIN_FILENO);
+
+            if (-1 == fd) {
+                perror("Error (open)");
+                exit(1);
+            }
+
+            if (dup2(fd, STDIN_FILENO)) {
+                perror("Error (dup2)");
+                exit(1);
+            }
         }
 
         // Output redirection
         if (cmd->out) {
             // 0666 = read/write for user (normal)
             int fd = open(cmd->out, O_CREAT | O_WRONLY, 0666);
-            dup2(fd, STDOUT_FILENO);
+
+            if (-1 == fd) {
+                perror("Error (open)");
+                exit(1);
+            }
+
+            if (dup2(fd, STDOUT_FILENO)) {
+                perror("Error (dup2)");
+                exit(1);
+            }
         }
 
         // Execute the command.
-        int status = execv(cmd->prog, cmd->args);
+        execvp(cmd->prog, cmd->args); /* Only returns error*/
+        perror("Error (exec)");
+        exit(1);
     } else { /* Parent */
         waitpid(pid, NULL, 0);
     }
 }
 
-void shell()
+void repl()
 {
     char *input;
     struct command cmd;
+
+    // Set the path to something reasonable, in the event that
+    // the shell is not launched inside another shell.
+    setenv("PATH", "/usr/local/bin:/usr/bin:/bin", 1);
 
     while ((input = readln("$ ", NULL))) {
         scan(&cmd, input);
@@ -58,11 +88,21 @@ void shell()
     printf("\n(exit)\n");
 }
 
-int main()
+int main(int nargs, char **args)
 {
-    // Enter raw mode.
-    raw_mode();
-    // Run the shell.
-    shell();
+    if (nargs > 1) { /* Script */
+        char *filename = args[1];
+        printf("TODO: Run script %s\n", filename);
+    } else { /* Interactive */
+        // Trap SIGINT.
+        if (SIG_ERR == signal(SIGINT, SIG_IGN)) {
+            perror("Error (signal)");
+            exit(1);
+        }
+
+        // Run the repl.
+        repl();
+    }
+
     return 0;
 }
